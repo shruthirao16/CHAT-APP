51523fe11750a2e1747307d336ce6e3dd2f32e0a
tshruthirao <tshruthirao16@gmail.com>
2025-04-24 01:07:28 +0530

    Updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,29 @@
 import express from "express"
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js"
 
-
+ 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
-
-app.use(express.json);
+const PORT = 4000;
+app.use(express.json()); 
+app.use(cookieParser());
+ 
+app.get("/", (req, res) => {
+    res.send("Server is alive!");
+});
 
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => {
-    console.log("Server is running on PORT "+PORT);
-    connectDB();
-})    
+
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
+
+  

4c2609d170f4824397b25ccfadeb289647b11bf8
tshruthirao <tshruthirao16@gmail.com>
2025-04-24 01:05:09 +0530

    Updated db and utils

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,16 @@
-export const generateToken= (userID, res)=>{
-    
+import jwt from "jsonwebtoken"
+export const generateToken= (userId, res)=>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:"7d"
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "Strict",
+        secure: process.env.NODE_ENV !== "development"
+    });
+    res.status(200).json({
+        message: "Token generated and cookie set",
+    });
 }

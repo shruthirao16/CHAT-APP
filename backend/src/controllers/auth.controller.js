b0be692893b5a768e84b828a3297b019c7472220
tshruthirao <tshruthirao16@gmail.com>
2025-04-25 23:16:51 +0530

    Modified with checkAuth function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs" 
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async(req, res)=>{
     const {fullname,email,password} = req.body
@@ -106,5 +107,11 @@ export const updateProfile = async(req, res) =>{
       }
 }
 
-
-
+export const checkAuth = (req, res) => {
+    try {
+      res.status(200).json(req.user);
+    } catch (error) {
+      console.log("Error in checkAuth controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
